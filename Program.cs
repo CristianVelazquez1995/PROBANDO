4f036c4d6ad5e6b403ff11a71581a5cc2603304e
CristianVelazquez1995 <cristian1995@gmail.com>
2021-04-14 20:42:51 -0300

    algo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace probandp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
